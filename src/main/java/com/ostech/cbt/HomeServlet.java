e460b3a8a44260640099c4324c71a251109d71df
Osaremhen Ukpebor <osas.solo@outlook.com>
2023-03-10 15:11:00 +0100

    Updated HomeServlet

---
+++ src/main/java/com/ostech/cbt/HomeServlet.java
@@ -0,0 +1,32 @@
+package com.ostech.cbt;
+
+import com.ostech.cbt.database.SubjectManipulator;
+import com.ostech.cbt.model.Subject;
+
+import java.io.*;
+import java.util.ArrayList;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(urlPatterns = {"/", "/home", "/index"})
+public class HomeServlet extends HttpServlet {
+    private ArrayList<Subject> subjects;
+    public void init() {
+        subjects = SubjectManipulator.getAllSubjects();
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        response.setContentType("text/html");
+
+        for (Subject currentSubject: subjects) {
+            System.out.println(currentSubject.getName());
+        }
+
+        request.setAttribute("subjects", subjects);
+        request.getRequestDispatcher("index.jsp").forward(request, response);
+    }
+
+    public void destroy() {
+    }
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,13 +1,28 @@
+<%@ page import="java.util.ArrayList" %>
+<%@ page import="com.ostech.cbt.model.Subject" %>
 <%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
 <!DOCTYPE html>
 <html>
 <head>
-    <title>JSP - Hello World</title>
+    <title>CBT</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="css/bootstrap.css">
 </head>
 <body>
 <h1><%= "Hello World!" %>
 </h1>
 <br/>
-<a href="hello-servlet">Hello Servlet</a>
+
+<%
+    ArrayList<Subject> subjects = (ArrayList<Subject>) request.getAttribute("subjects");
+
+    for (Subject currentSubject: subjects) {
+%>
+    <p><%= currentSubject.getName()%></p>
+<%
+    }
+%>
+<a href="${pageContext.request.contextPath}/">Hello Servlet</a>
 </body>
 </html>
