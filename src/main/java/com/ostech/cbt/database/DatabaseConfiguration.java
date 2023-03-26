4509689bf87246dce1713d4110da0554ffb955a4
Osaremhen Ukpebor <osas.solo@outlook.com>
2023-03-26 22:03:16 +0100

    Updated DatabaseConfiguration

---
+++ src/main/java/com/ostech/cbt/database/DatabaseConfiguration.java
@@ -0,0 +1,31 @@
+package com.ostech.cbt.database;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DatabaseConfiguration {
+
+    public Connection getDatabaseConnection() {
+        try {
+            final File CURRENT_DIRECTORY = new File(this.getClass().getProtectionDomain().getCodeSource().getLocation().getPath());
+            final Dotenv dotenv = Dotenv.configure().directory(CURRENT_DIRECTORY.getCanonicalPath()).load();
+            final String DATABASE_HOST = dotenv.get("DATABASE_HOST");
+            final String DATABASE_PASSWORD = dotenv.get("DATABASE_PASSWORD");
+            final String DATABASE_USER = dotenv.get("DATABASE_USER");
+            final String DATABASE_NAME = dotenv.get("DATABASE_NAME");
+
+            String driver = "com.mysql.cj.jdbc.Driver";
+            Class.forName(driver);
+            String url = String.format("jdbc:mysql://%s:3306/%s", DATABASE_HOST, DATABASE_NAME);
+
+            return DriverManager.getConnection(url, DATABASE_USER, DATABASE_PASSWORD);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+}
