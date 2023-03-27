1eb40d3d644490d0e065df6421f6108c5a234212
Osaremhen Ukpebor <osas.solo@outlook.com>
2023-03-27 13:34:25 +0100

    Updated SubjectServlet

--- src/main/java/com/ostech/cbt/SubjectServlet.java
+++ src/main/java/com/ostech/cbt/SubjectServlet.java
@@ -4,12 +4,13 @@ import com.ostech.cbt.database.SubjectManipulator;
 import com.ostech.cbt.model.Candidate;
 import com.ostech.cbt.model.Subject;
 
-import javax.servlet.*;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 @WebServlet(name = "SubjectServlet", urlPatterns = {"/subject"})
 public class SubjectServlet extends HttpServlet {
