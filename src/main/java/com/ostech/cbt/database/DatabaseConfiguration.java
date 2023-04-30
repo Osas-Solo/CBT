6dd77b58abb2e81422868565763a22f554208f9f
Osaremhen Ukpebor <osas.solo@outlook.com>
2023-04-30 23:33:55 +0100

    Updated database configuration

--- src/main/java/com/ostech/cbt/database/DatabaseConfiguration.java
+++ src/main/java/com/ostech/cbt/database/DatabaseConfiguration.java
@@ -10,11 +10,10 @@ public class DatabaseConfiguration {
 
     public Connection getDatabaseConnection() {
         try {
-            final Dotenv dotenv = Dotenv.load();
-            final String DATABASE_HOST = dotenv.get("DATABASE_HOST");
-            final String DATABASE_PASSWORD = dotenv.get("DATABASE_PASSWORD");
-            final String DATABASE_USER = dotenv.get("DATABASE_USER");
-            final String DATABASE_NAME = dotenv.get("DATABASE_NAME");
+            final String DATABASE_HOST = System.getenv("DATABASE_HOST");
+            final String DATABASE_PASSWORD = System.getenv("DATABASE_PASSWORD");
+            final String DATABASE_USER = System.getenv("DATABASE_USER");
+            final String DATABASE_NAME = System.getenv("DATABASE_NAME");
 
             String driver = "com.mysql.cj.jdbc.Driver";
             Class.forName(driver);
